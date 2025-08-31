83895d08544ed2996e2c7925de326e8d4aeef851
Emmanuel Moses <emmanuel5vohd@gmail.com>
2025-08-31 13:01:00 +0100

    New Commit

---
+++ script.js
@@ -0,0 +1,149 @@
+const taskInput = document.getElementById("task-input");
+const addTaskButton = document.getElementById("add-task-btn");
+const taskList = document.getElementById("task-list");
+const emptyImage = document.querySelector(".empty-image");
+const toDoContainer = document.querySelector(".todos-container");
+const progressBar = document.getElementById("progress");
+const progressNumbers = document.getElementById("numbers");
+
+const toggleEmptyState = () => {
+  emptyImage.style.display = taskList.children.length === 0 ? "block" : "none";
+  toDoContainer.style.width = taskList.children.length > 0 ? "100%" : "100%";
+};
+const updateProgress = (checkCompletion = true) => {
+  const totalTasks = taskList.children.length;
+  const completedTasks = taskList.querySelectorAll(".checkbox:checked").length;
+  progressBar.style.width = totalTasks
+    ? `${(completedTasks / totalTasks) * 100}%`
+    : "0%";
+  progressNumbers.textContent = `${completedTasks} / ${totalTasks}`;
+  if (checkCompletion && totalTasks > 0 && completedTasks === totalTasks) {
+    confettiAnimation();
+  }
+};
+const saveTaskToLocalSTorage = () => {
+  const tasks = Array.from(
+    taskList.querySelectorAll("li").map((li) => ({
+      text: li.querySelector("span").textContent,
+      completed: li.querySelector(".checkbox").checked,
+    }))
+  );
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+const loadTasksFromLocalStorage = () => {
+  const savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  savedTasks.forEach(({ text, completed }) => addTask(text, completed, false));
+  toggleEmptyState();
+  updateProgress();
+};
+const addTask = (text, completed = false, checkCompletion = true) => {
+  const taskText = text || taskInput.value.trim();
+  if (!taskText) {
+    return;
+  }
+  const li = document.createElement("li");
+  li.innerHTML = `
+  <input type="checkbox" class="checkbox" ${completed ? "checked" : ""}/>
+  <span>${taskText}</span>
+  <div class="task-buttons">
+  <button class="edit-btn"><i class="fa-solid fa-pen"></i></button>
+  <button class="delete-btn"><i class="fa-solid fa-trash"></i></button>
+  </div>
+  `;
+  const checkbox = li.querySelector(".checkbox");
+  const editBtn = li.querySelector(".edit-btn");
+  const span = li.querySelector("span");
+  if (completed) {
+    li.classList.add("completed");
+    editBtn.disabled = true;
+    editBtn.style.opacity = "0.5";
+    editBtn.style.pointerEvents = "none";
+    span.style.textDecoration = "2px line-through #000";
+    span.style.color = "#000";
+    span.style.opacity = "0.5";
+  }
+  checkbox.addEventListener("change", () => {
+    const isChecked = checkbox.checked;
+    li.classList.toggle("completed", isChecked);
+    editBtn.disabled = isChecked;
+    editBtn.style.opacity = isChecked ? "0.5" : "1";
+    editBtn.style.pointerEvents = isChecked ? "none" : "auto";
+    span.style.textDecoration = isChecked ? "2px line-through #000" : "none";
+    span.style.color = isChecked ? "#000" : "#fff";
+    span.style.opacity = isChecked ? "0.5" : "1";
+    updateProgress();
+    saveTaskToLocalSTorage();
+  });
+  editBtn.addEventListener("click", () => {
+    if (!checkbox.checked) {
+      taskInput.value = li.querySelector("span").textContent;
+      li.remove();
+      toggleEmptyState();
+      updateProgress(false);
+      saveTaskToLocalSTorage();
+    }
+  });
+  li.querySelector(".delete-btn").addEventListener("click", () => {
+    li.remove();
+    toggleEmptyState();
+    updateProgress();
+    saveTaskToLocalSTorage();
+  });
+  taskList.appendChild(li);
+  taskInput.value = "";
+  toggleEmptyState();
+  updateProgress(checkCompletion);
+  saveTaskToLocalSTorage();
+};
+
+addTaskButton.addEventListener("click", () => addTask());
+taskInput.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    addTask();
+  }
+});
+
+loadTasksFromLocalStorage();
+
+const confettiAnimation = () => {
+  const count = 200,
+    defaults = {
+      origin: { y: 0.7 },
+    };
+
+  function fire(particleRatio, opts) {
+    confetti(
+      Object.assign({}, defaults, opts, {
+        particleCount: Math.floor(count * particleRatio),
+      })
+    );
+  }
+
+  fire(0.25, {
+    spread: 26,
+    startVelocity: 55,
+  });
+
+  fire(0.2, {
+    spread: 60,
+  });
+
+  fire(0.35, {
+    spread: 100,
+    decay: 0.91,
+    scalar: 0.8,
+  });
+
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 25,
+    decay: 0.92,
+    scalar: 1.2,
+  });
+
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 45,
+  });
+};
